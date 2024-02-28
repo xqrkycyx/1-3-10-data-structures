e586917207db4b58320c3da6e369ed23656c513d
Will Williams <wwilliams@indeed.com>
2024-02-27 18:14:43 -0800

    Req 5: Implement algorithm to check if string is palindrome using Stack

--- src/stack/isPalindrome.js
+++ src/stack/isPalindrome.js
@@ -1,24 +1,32 @@
-/**
- * A palindrome is a word, phrase, or number that is spelled the same forward and backward.
- * For example, “dad” is a palindrome; “A man, a plan, a canal: Panama” is a palindrome if you take out the spaces and ignore the punctuation;
- * and 1,001 is a numeric palindrome.
- *
- * Use a stack to determine whether or not a given string is a palindrome.
- *
- * The implementation should have O(n) performance.
- *
- * @param text
- *  a possibly empty string that may be a palindrome.
- */
+const Stack = require("../linked-list/stack");
 
-const Stack = require("../stack/stack");
-
+// TODO: Write an O(n) algorithm that uses a stack to determine whether the given input text is palindrome or not.
 function isPalindrome(text) {
-  const cleanText = text.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  text = text.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  if (text.length === 0) return false;
 
-  // TODO: Write an O(n) algorithm that uses a stack to determine whether the given input text is palindrome or not.
+  const stack = new Stack();
 
-  return false;
+  const firstHalfLength = Math.floor(text.length / 2);
+
+  // if string has a middle character, remove it - doesn't affect palindrome
+  if (text.length % 2) {
+    text = text.slice(0, firstHalfLength) + text.slice(firstHalfLength + 1);
+  }
+
+  // loop through text putting the first half into a stack, and then comparing the second half
+  for (let i = 0; i < text.length; i++) {
+    if (i < firstHalfLength) {
+      stack.push(text[i]);
+    }
+    if (i >= firstHalfLength) {
+      if (stack.pop() !== text[i]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
 
 module.exports = isPalindrome;
